eaceefc6ed0e8d47cf547d2ee63c6e003ac29771
axelwp <axelwpothier@gmail.com>
2026-04-17 13:03:10 +0900

    feat: added render pipeline and large triangle canvas to use with raymarching

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import './style.css'
+import shaderSource from './shaders/fullscreen.wgsl?raw' 
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 console.log(canvas)
-const {device, context} = await init(canvas)
-render(device, context)
+const {device, context, pipeline} = await init(canvas)
+render(device, context, pipeline)
 
 async function init(canvas:HTMLCanvasElement){
   if(!navigator.gpu)
@@ -17,17 +18,26 @@ async function init(canvas:HTMLCanvasElement){
 
   const context = canvas.getContext('webgpu') as GPUCanvasContext
 
-  context.configure({device, format: navigator.gpu.getPreferredCanvasFormat(), alphaMode: 'premultiplied'})
+  const format = navigator.gpu.getPreferredCanvasFormat()
+
+  context.configure({device, format: format, alphaMode: 'premultiplied'})
 
   canvas.width = canvas.clientWidth * devicePixelRatio
   canvas.height = canvas.clientHeight * devicePixelRatio
 
-  return{ device, context }
+  const module = device.createShaderModule({code: shaderSource})
+  const pipeline = device.createRenderPipeline({layout: 'auto', vertex: { module, entryPoint: 'vs' }, fragment: { module, entryPoint: 'fs', targets: [{ format }]}, primitive: {topology: 'triangle-list' } })
+
+  return{ device, context, pipeline }
 }
 
-function render(device: GPUDevice, context:GPUCanvasContext){
+function render(device: GPUDevice, context:GPUCanvasContext, pipeline:GPURenderPipeline){
   const encoder = device.createCommandEncoder()
   const pass = encoder.beginRenderPass({ colorAttachments: [{view: context.getCurrentTexture().createView(), clearValue: {r: 0.1, g: 0.2, b: 0.3, a: 1}, loadOp: 'clear', storeOp: 'store'}] })
+
+  pass.setPipeline(pipeline)
+  pass.draw(3)
+
   pass.end()
   device.queue.submit([encoder.finish()])
 }
